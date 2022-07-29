befef8596a53f28c786fc33697747fb52d32aba5
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-07-29 02:02:17 -0300

    Teste tamanho nome na classe de comando CreateBoleto

---
+++ PaymentContext.MSTest.Tests/Commands/CreateBoletoSubscriptionCommandTest.cs
@@ -0,0 +1,18 @@
+﻿using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.MSTest.Tests.Commands;
+
+[TestClass]
+public class CreateBoletoSubscriptionCommandTest
+{
+    [TestMethod]
+    public void ShouldReturnErrorWhenNameIsInvalid()
+    {
+        var command = new CreateBoletoSubscriptionCommand();
+        command.FirstName = "Th";
+
+        command.Validate();
+
+        Assert.IsTrue(!command.IsValid);
+    }
+}
